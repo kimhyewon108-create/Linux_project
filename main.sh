ef162cd92fc52f099528cefd6485c98607279366
JiYou <ailris@naver.com>
2025-12-02 20:49:18 +0900

    ui 1st commit

---
+++ main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while true
+do
+    echo "1) 환경 변수 확인"
+    echo "2) 네트워크 상태 확인"
+    echo "3) 프로세스 목록 확인"
+    echo "4) 현재 경로 확인"
+    echo "5) 종료"
+    echo -n "번호를 입력하세요: "
+    read choice
+done
